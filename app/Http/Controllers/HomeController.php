8f2deae99dacd3adf1c434cbd1bb79c1608e8ffe
Nerijus <krekas@gmail.com>
2021-02-09 11:46:13 +0200

    More front

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke() {
-        return view('front.home');
+        return view('front.home', [
+            'books' => Book::approved()->simplePaginate(25)
+        ]);
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Author;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
     use HasFactory;
+
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    public function scopeApproved($query)
+    {
+        return $query->where('approved', '1');
+    }
 }
