4b415dcb52f2db5d1edc684b995c150cefcdfc28
krekas <krekas@gmail.com>
2021-02-12 22:52:24 +0200

    Small design fixes

--- resources/views/front/user/settings.blade.php
+++ resources/views/front/user/settings.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <section class="text-gray-600 body-font">
+    <section class="text-gray-600 body-font mb-5 md:mb-0">
         <div class="container max-w-7xl px-5 mx-auto mt-6">
             <x-alert />
 
@@ -43,7 +43,7 @@
                 </div>
               </div>
 
-              <div>
+              <div class="mt-5 sm:mt-0">
                 <div class="md:grid md:grid-cols-3 md:gap-6">
                   <div class="md:col-span-1">
                     <div class="px-4 sm:px-0">
