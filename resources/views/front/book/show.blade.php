0b62c4a4d2903bf8501c240e61e2099fdc091acd
krekas <krekas@gmail.com>
2021-02-15 22:41:31 +0200

    Info message for unapproved book

--- resources/views/front/book/show.blade.php
+++ resources/views/front/book/show.blade.php
@@ -2,6 +2,19 @@
     <section class="text-gray-600 body-font">
         <div class="container max-w-7xl px-5 mx-auto mt-6">
 
+          @if (!$book->approved)
+          <div class="bg-blue-100 p-5 w-4/5 border-l-4 mx-auto border-blue-500 mb-4">
+              <div class="flex space-x-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                      class="flex-none fill-current text-blue-500 h-4 w-4">
+                      <path
+                          d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-.001 5.75c.69 0 1.251.56 1.251 1.25s-.561 1.25-1.251 1.25-1.249-.56-1.249-1.25.559-1.25 1.249-1.25zm2.001 12.25h-4v-1c.484-.179 1-.201 1-.735v-4.467c0-.534-.516-.618-1-.797v-1h3v6.265c0 .535.517.558 1 .735v.999z" />
+                  </svg>
+                  <div class="flex-1 leading-tight text-sm text-blue-700">This book still needs to be approved</div>
+              </div>
+          </div>
+          @endif
+
               <div class="lg:w-4/5 mx-auto flex flex-wrap">
                 <img alt="ecommerce" class="lg:w-1/3 w-full h-64 object-cover object-center rounded" src="https://dummyimage.com/330x400">
                 <div class="lg:w-2/3 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
