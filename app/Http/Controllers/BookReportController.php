c4ec724a89fbcf94d2f2f007dd0290a6cfdb5a56
krekas <nerijus.bukartas@gmail.com>
2021-03-12 18:37:49 +0200

    Use laravel notifications for book report

--- app/Http/Controllers/BookReportController.php
+++ app/Http/Controllers/BookReportController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\CreateReportRequest;
 use App\Mail\ReportBook;
 use App\Models\Book;
 use App\Models\User;
-use Illuminate\Support\Facades\Mail;
+use App\Notifications\SendReportNotification;use Illuminate\Support\Facades\Mail;use Illuminate\Support\Facades\Notification;
 
 class BookReportController extends Controller
 {
@@ -17,10 +17,9 @@ class BookReportController extends Controller
 
     public function store(CreateReportRequest $request, Book $book)
     {
-        $adminMail = User::firstWhere('admin', true)->email;
+        $admins = User::where('admin', true)->get();
 
-        Mail::to($adminMail)
-            ->send(new ReportBook($request->report, $book));
+        Notification::send($admins, new SendReportNotification($request->report, $book));
 
         return redirect()->route('books.show', $book)->with('success', 'Report sent successfully.');
     }
