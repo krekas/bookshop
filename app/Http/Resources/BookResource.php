f3d2d3cf914c1d17100a94a4dc0520d6cc2ed2c7
Nerijus <nerijus.bukartas@gmail.com>
2021-05-13 19:36:03 +0300

    Update BookResource.php

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class BookResource extends JsonResource
 {
